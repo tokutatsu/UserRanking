8cd32e0962b52ba81cd484e8fbca7338cf1fc274
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2019-11-15 14:55:58 +0900

    ランキングを昇順、降順で返す処理を実装

--- routes/rankings.js
+++ routes/rankings.js
@@ -2,13 +2,31 @@ const express = require('express');
 const router = express.Router();
 const models = require('../models/index.js');
 
-router.get('/', (req, res, next) => {
-    models.Ranking.findAll({}).then(ranking => {
+// スコアランキングを降順で返す
+router.get('/desc', (req, res, next) => {
+    models.Ranking.findAll({
+        order: [
+            ['user_score', 'DESC']
+        ]
+    }).then((ranking) => {
         res.send(ranking);
         next();
     });
 });
 
+// スコアランキングを昇順で返す
+router.get('/asc', (req, res, next) => {
+    models.Ranking.findAll({
+        order: [
+            ['user_score', 'ASC']
+        ]
+    }).then((ranking) => {
+        res.send(ranking);
+        next();
+    });
+});
+
+// ランキングにユーザを登録する
 router.post('/', (req, res, next) => {
     models.Ranking.create({
         user_name: req.body.name,
